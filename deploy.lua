e641277bf3b053336c47faa38cc2f5dc166b1f43
aghagolz <mohammad.aghagolzadeh@here.com>
2017-02-27 17:40:45 -0600

    reducing the parameters for testing

--- deploy.lua
+++ deploy.lua
@@ -26,95 +26,30 @@ model_bin   = "bin/rnnTracker_r300_l1_n1_m1_d4.t7"
 ------------------------------------------------------------
 -- global variables:
 opt = {
-  model_split=0,
-  dalink=1,
   max_n=1,
-  freq_reshuffle=0,
-  init_from="",
-  plot_every=10000,
+  max_m=1,
   statePredIndex2=3,
-  det_false=0.2,
   rnn_size=300,
-  lrng_rate=3e-05,
-  dropout=0.03,
-  random_epoch=0,
-  seed=3,
-  kappa=1,
-  miss_thr=0.05,
-  synth_valid=1,
   verbose=2,
   exPredIndex=4,
-  det_thr=0.03,
-  order_dets=0,
-  lambda=1,
-  einp=1,
   use_gt_input=0,
-  xi=1,
-  norm_mean=1,
-  exSmoothPredIndex=5,
   real_data=1,
-  fixed_n=1,
-  nu=0.5,
-  max_epochs=100000,
   batch_size=1,
-  reshuffle_dets=0,
-  pert_training=0,
-  det_noise=0.01,
-  synth_training=1,
-  forget_bias=1,
-  eval_conf="config/XPOS.ini",
-  modelParams={
-    [1]="model_index",
-    [2]="rnn_size",
-    [3]="num_layers",
-    [4]="max_n",
-    [5]="max_m",
-    [6]="state_dim" },
-  lrng_rate_decay_after=10000,
-  linp=0,
-  trim_tracks=1,
+  reshuffle_dets=0, -- wow... don't mess with this one
   use_da_input=4,
-  training_file="",
   ex_thr=0.5,
-  loss_type=1,
-  full_set=1,
-  opencl=0,
   statePredIndex=2,
-  detlink=1,
-  one_hot=1,
-  max_m=1,
-  gpuid=-1,
-  max_nf=0,
-  decay_rate=0.97,
-  suppress_x=0,
-  rand_par_rng=0.08,
-  config="config/0314Ea-3.txt",
+  gpuid=0,
   mini_batch_size=1,
-  dummy_weight=1,
-  grad_clip=0.1,
-  save_plots=0,
-  dummy_det_val=0,
-  print_every=1000,
-  norm_std=-1,
+  norm_std=-1, -- also don't mess with this
   num_layers=1,
-  eval_mot15=1,
-  model_index=3,
   mu=0,
-  det_fail=0.1,
-  vel=0,
   profiler=0,
   real_dets=1,
-  daPredIndex=4,
-  temp_win=300,
-  model="rnn",
-  eval_val_every=10000,
+  temp_win=50,
   state_dim=4,
-  lrng_rate_decay=0.95
 }
-imSizes = {
-  Synth={imH=1000,imW=1000},
-  [seq_name]={imH=375,imW=1242}
-}
+imSizes = {[seq_name]={imH=375,imW=1242}}
 updLoss = opt.kappa ~= 0
 predLoss = opt.lambda ~= 0
 daLoss = opt.mu ~= 0
@@ -133,7 +68,7 @@ T = opt.temp_win - opt.batch_size
 ------------------------------------------------------------
 -- torch configuration:
 torch.setdefaulttensortype('torch.FloatTensor')
-torch.manualSeed(opt.seed)
+torch.manualSeed(12)
 ------------------------------------------------------------
 -- load the model:
 checkpoint = torch.load(model_bin)
@@ -141,10 +76,16 @@ protos = checkpoint.protos
 protos.rnn:evaluate() -- TESTING mode
 ------------------------------------------------------------
 -- read data:
-realTracksTab, realDetsTab, realLabTab, realExTab, realDetExTab, realSeqNames = prepareData('real', {seq_name}, {seq_name},  true)
 AllTracksTab, AllDetsTab, AllLabTab, AllExTab, AllDetExTab  = prepareData('real', {seq_name}, {seq_name},  true)
 ------------------------------------------------------------
 -- some more fucking global variables:
+realTracksTab = AllTracksTab
+realDetsTab = AllDetsTab
+realLabTab = AllLabTab
+realExTab = AllExTab
+realDetExTab = AllDetExTab
+realSeqNames = {{seq_name}}
+
 tracks = realTracksTab[1]
 detections = realDetsTab[1]:clone()
 labels = realLabTab[1]:clone() -- only for debugging
@@ -275,43 +216,43 @@ writeResTensor = writeResTensor:sub(1,maxAllTargets)
 writeTXT(writeResTensor, 'out/out.txt')
 ------------------------------------------------------------
 -- plot tracks over detections:
--- fixedTracks = torch.zeros(1,F,D)
--- fixedEx = torch.zeros(1,F):int()
--- for tar=1,N do
---   started, finished=0,0
---   for t=1,F do
---     if (t==1 and predExBin[tar][t]==1) or (t>1 and predExBin[tar][t]==1 and predExBin[tar][t-1]==0) then
---       started=t
---     end
---     if (t==F and predExBin[tar][t]==1) or (t<F and predExBin[tar][t]==1 and predExBin[tar][t+1]==0) then
---       finished=t
---     end
---     if started>0 and finished>0 then
---       tmpTrack = torch.zeros(1,F,D)
---       tmpTrack[{{1},{started,finished},{}}] = finalTracks[{{tar},{started,finished},{}}]
---       fixedTracks=fixedTracks:cat(tmpTrack,1)
+fixedTracks = torch.zeros(1,F,D)
+fixedEx = torch.zeros(1,F):int()
+for tar=1,N do
+  started, finished=0,0
+  for t=1,F do
+    if (t==1 and predExBin[tar][t]==1) or (t>1 and predExBin[tar][t]==1 and predExBin[tar][t-1]==0) then
+      started=t
+    end
+    if (t==F and predExBin[tar][t]==1) or (t<F and predExBin[tar][t]==1 and predExBin[tar][t+1]==0) then
+      finished=t
+    end
+    if started>0 and finished>0 then
+      tmpTrack = torch.zeros(1,F,D)
+      tmpTrack[{{1},{started,finished},{}}] = finalTracks[{{tar},{started,finished},{}}]
+      fixedTracks=fixedTracks:cat(tmpTrack,1)
 
---       started=0
---       finished=0
---     end
---   end
--- end
+      started=0
+      finished=0
+    end
+  end
+end
 
--- if fixedTracks:size(1)>1 then
---   fixedTracks=fixedTracks:sub(2,-1)
--- else
---   fixedTracks=finalTracks:clone()
--- end
+if fixedTracks:size(1)>1 then
+  fixedTracks=fixedTracks:sub(2,-1)
+else
+  fixedTracks=finalTracks:clone()
+end
 
 trueDets = alldetexlabels:reshape(maxAllDets, opt.temp_win, 1):expand(maxAllDets, opt.temp_win, stateDim)
 realDets = alldetections:clone():cmul(trueDets:float())
 
 plotTab = {}
 plotTab = getDetectionsPlotTab(realDets, plotTab, nil, nil)
--- plotTab = getTrackPlotTab(fixedTracks, plotTab, 2,nil,nil,1)
-plotTab = getTrackPlotTab(finalTracks, plotTab, 2,nil,nil,1)
+plotTab = getTrackPlotTab(fixedTracks, plotTab, 2,nil,nil,1)
+-- plotTab = getTrackPlotTab(finalTracks, plotTab, 2,nil,nil,1)
 
-plot(plotTab, nil, 'out/out.png', nil, true)
+plot(plotTab, nil, 'out/out.png', nil, false)
 ------------------------------------------------------------
 
 
